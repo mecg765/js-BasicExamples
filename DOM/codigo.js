d0e6c4f9985e23d728f4b33a95e59f09a8b2e433
Monica <m.e.c.g_15@hotmail.com>
2022-02-05 16:19:18 -0600

    Ejemplo DOM

---
+++ DOM/codigo.js
@@ -0,0 +1,4 @@
+var path = window.location;
+console.log("Este es un mensaje en la consola.");
+document.write("Este es un mensaje desde el código de JavaScript: ");
+document.write("Estás en " + path);
